b97515e34bd3d0bdfd87a35f6271183ccf0a906c
ronyrtan <ron.yr.tan@hotmail.com>
2016-10-14 01:57:07 +1100

    cloudinary fixing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :username, :name, :password, :password_confirmation, :profile_pic)
+    params.require(:user).permit(:email, :username, :name, :password, :password_confirmation)
   end
 end
