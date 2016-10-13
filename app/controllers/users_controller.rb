878847461090e225d5e910775cb895939e4c3297
ronyrtan <ron.yr.tan@hotmail.com>
2016-10-14 01:00:12 +1100

    tried redoing cloud_names and keys

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to whistles_path # Sign up was successful
-    else
-      render :new
+    if params [:file].present?
+      req = Cloudinary::Uploader.upload params[:file]
+      user.profile_pic = req["public_id"]
+      user.save
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to whistles_path # Sign up was successful
+      else
+        render :new
+      end
     end
   end
 
@@ -34,10 +39,18 @@ class UsersController < ApplicationController
 
   def edit
     @user = @current_user
+    # @user = User.find params[:id]
   end
 
   def update
-    @user = @current_user
+    # @user = @current_user
+    @user = User.find params[:id]
+    if params [:file].present?
+      req = Cloudinary::Uploader.upload params[:file]
+      user.profile_pic = req["public_id"]
+    end
+    @user.update_attributes user_params
+    @user.save
     if @user.update(user_params)
       flash[:message] = 'Profile successfully updated'
       redirect_to @user
